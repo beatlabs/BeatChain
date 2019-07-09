b953f384d2983bd9faefe2007840f64cd244c559
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2019-07-09 16:25:56 +0300

    Initial commit

---
+++ BeatChain/BeatChain.h
@@ -0,0 +1,19 @@
+//
+//  BeatChain.h
+//  BeatChain
+//
+//  Created by Spiros Gerokostas on 09/07/2019.
+//  Copyright © 2019 Spiros Gerokostas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BeatChain.
+FOUNDATION_EXPORT double BeatChainVersionNumber;
+
+//! Project version string for BeatChain.
+FOUNDATION_EXPORT const unsigned char BeatChainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeatChain/PublicHeader.h>
+
+
